1317c1e3c513e6982399ec7edb440cae30799a83
Dimos <dn@oawms.com>
2025-01-27 10:54:29 +0200

    Commit version 0.002 from 27.01.2025

--- laravel/resources/views/vendor/platform/footer.blade.php
+++ laravel/resources/views/vendor/platform/footer.blade.php
@@ -4,7 +4,7 @@
 
     <div class="text-center user-select-none my-4 d-none d-lg-block">
         <p class="small mb-0">
-            (с) Copyright by NWMS. Version of deploy 0.001 from 19.01.2025
+            (с) Copyright by NWMS. Version of deploy 0.002 from 27.01.2025
         </p>
     </div>
 @endguest
