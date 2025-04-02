d49f22f24a5e1c9ef20e7818a9e8d1bf0bcf671b
Dimos <dn@oawms.com>
2025-04-02 17:13:26 +0300

    Add new helps, plus new img. Change mobile version.

--- laravel/resources/views/Site/MainBlocks/docsBlocksTheory.blade.php
+++ laravel/resources/views/Site/MainBlocks/docsBlocksTheory.blade.php
@@ -5,11 +5,11 @@
     <!-- widget recent post -->
     <div class="widget-recent-post">
         <div class="rpost-thumb">
-            <a href="{{ $lang_str }}/docs/theory"><img src="/img/docs/aboutWMS.jpg" alt="{{ CustomSiteTranslator::get('Theory, about WMS', $lang) }}" style="width: 75px;"></a>
+            <a href="{!! $lang_str !!}/docs/theory"><img src="/img/docs/aboutWMS.jpg" alt="{!! CustomSiteTranslator::get('Theory, about WMS', $lang) !!}" style="width: 75px;"></a>
         </div>
         <div class="rpost-content">
             <div class="rpost-title">
-                <h4><a href="{{ $lang_str }}/docs/theory">{{ CustomSiteTranslator::get('Theory (beginning)', $lang) }}</a></h4>
+                <h4><a href="{!! $lang_str !!}/docs/theory">{!! CustomSiteTranslator::get('Theory (beginning)', $lang) !!}</a></h4>
                 <span>30.03.2025</span>
             </div>
         </div>
@@ -17,11 +17,11 @@
     <!-- widget recent post -->
     <div class="widget-recent-post">
         <div class="rpost-thumb">
-            <a href="{{ $lang_str }}/docs/theory/receiving_goods"><img src="/img/docs/receivingGoods_mult.jpg" alt="{{ CustomSiteTranslator::get('Receiving goods', $lang) }}" style="width: 75px;"></a>
+            <a href="{!! $lang_str !!}/docs/theory/receiving_goods"><img src="/img/docs/receivingGoods_mult.jpg" alt="{!! CustomSiteTranslator::get('Receiving goods', $lang) !!}" style="width: 75px;"></a>
         </div>
         <div class="rpost-content">
             <div class="rpost-title">
-                <h4><a href="{{ $lang_str }}/docs/theory/receiving_goods">{{ CustomSiteTranslator::get('Receiving goods', $lang) }}</a></h4>
+                <h4><a href="{!! $lang_str !!}/docs/theory/receiving_goods">{!! CustomSiteTranslator::get('Receiving goods', $lang) !!}</a></h4>
                 <span>30.03.2025</span>
             </div>
         </div>
@@ -29,11 +29,11 @@
     <!-- widget recent post -->
     <div class="widget-recent-post">
         <div class="rpost-thumb">
-            <a href="{{ $lang_str }}/docs/theory/putaway_of_goods"><img src="/img/docs/putaway_of_goods_mult.jpg" alt="{{ CustomSiteTranslator::get('Putaway of goods on shelves', $lang) }}" style="width: 75px;"></a>
+            <a href="{!! $lang_str !!}/docs/theory/putaway_of_goods"><img src="/img/docs/putaway_of_goods_mult.jpg" alt="{!! CustomSiteTranslator::get('Putaway of goods on shelves', $lang) !!}" style="width: 75px;"></a>
         </div>
         <div class="rpost-content">
             <div class="rpost-title">
-                <h4><a href="{{ $lang_str }}/docs/theory/putaway_of_goods">{{ CustomSiteTranslator::get('Putaway of goods on shelves', $lang) }}</a></h4>
+                <h4><a href="{!! $lang_str !!}/docs/theory/putaway_of_goods">{!! CustomSiteTranslator::get('Putaway of goods on shelves', $lang) !!}</a></h4>
                 <span>30.03.2025</span>
             </div>
         </div>
@@ -41,11 +41,11 @@
     <!-- widget recent post -->
     <div class="widget-recent-post">
         <div class="rpost-thumb">
-            <a href="{{ $lang_str }}/docs/theory/warehouse_labeling"><img src="/img/docs/warehouse_labeling_mult.jpg" alt="{{ CustomSiteTranslator::get('Warehouse Labeling', $lang) }}" style="width: 75px;"></a>
+            <a href="{!! $lang_str !!}/docs/theory/warehouse_labeling"><img src="/img/docs/warehouse_labeling_mult.jpg" alt="{!! CustomSiteTranslator::get('Warehouse Labeling', $lang) !!}" style="width: 75px;"></a>
         </div>
         <div class="rpost-content">
             <div class="rpost-title">
-                <h4><a href="{{ $lang_str }}/docs/theory/warehouse_labeling">{{ CustomSiteTranslator::get('Warehouse Labeling', $lang) }}</a></h4>
+                <h4><a href="{!! $lang_str !!}/docs/theory/warehouse_labeling">{!! CustomSiteTranslator::get('Warehouse Labeling', $lang) !!}</a></h4>
                 <span>30.03.2025</span>
             </div>
         </div>
@@ -53,11 +53,11 @@
     <!-- widget recent post -->
     <div class="widget-recent-post">
         <div class="rpost-thumb">
-            <a href="{{ $lang_str }}/docs/theory/whats_next"><img src="/img/docs/whats_next_mult.jpg" alt="{{ CustomSiteTranslator::get("What's next?", $lang) }}" style="width: 75px;"></a>
+            <a href="{!! $lang_str !!}/docs/theory/whats_next"><img src="/img/docs/whats_next_mult.jpg" alt="{!! CustomSiteTranslator::get("What's next?", $lang) !!}" style="width: 75px;"></a>
         </div>
         <div class="rpost-content">
             <div class="rpost-title">
-                <h4><a href="{{ $lang_str }}/docs/theory/whats_next">{{ CustomSiteTranslator::get("What's next?", $lang) }}</a></h4>
+                <h4><a href="{!! $lang_str !!}/docs/theory/whats_next">{!! CustomSiteTranslator::get("What's next?", $lang) !!}</a></h4>
                 <span>30.03.2025</span>
             </div>
         </div>
@@ -65,11 +65,11 @@
     <!-- widget recent post -->
     <div class="widget-recent-post">
         <div class="rpost-thumb">
-            <a href="{{ $lang_str }}/docs/theory/orders"><img src="/img/docs/orders_mult.jpg" alt="{{ CustomSiteTranslator::get('Orders', $lang) }}" style="width: 75px;"></a>
+            <a href="{!! $lang_str !!}/docs/theory/orders"><img src="/img/docs/orders_mult.jpg" alt="{!! CustomSiteTranslator::get('Orders', $lang) !!}" style="width: 75px;"></a>
         </div>
         <div class="rpost-content">
             <div class="rpost-title">
-                <h4><a href="{{ $lang_str }}/docs/theory/orders">{{ CustomSiteTranslator::get("Orders", $lang) }}</a></h4>
+                <h4><a href="{!! $lang_str !!}/docs/theory/orders">{!! CustomSiteTranslator::get("Orders", $lang) !!}</a></h4>
                 <span>30.03.2025</span>
             </div>
         </div>
@@ -77,11 +77,11 @@
     <!-- widget recent post -->
     <div class="widget-recent-post">
         <div class="rpost-thumb">
-            <a href="{{ $lang_str }}/docs/theory/theory_again"><img src="/img/docs/sheme_eng3.jpg" alt="{{ CustomSiteTranslator::get('A bit of theory again', $lang) }}" style="width: 75px;"></a>
+            <a href="{!! $lang_str !!}/docs/theory/theory_again"><img src="/img/docs/sheme_eng3.jpg" alt="{!! CustomSiteTranslator::get('A bit of theory again', $lang) !!}" style="width: 75px;"></a>
         </div>
         <div class="rpost-content">
             <div class="rpost-title">
-                <h4><a href="{{ $lang_str }}/docs/theory/theory_again">{{ CustomSiteTranslator::get("A bit of theory again", $lang) }}</a></h4>
+                <h4><a href="{!! $lang_str !!}/docs/theory/theory_again">{!! CustomSiteTranslator::get("A bit of theory again", $lang) !!}</a></h4>
                 <span>30.03.2025</span>
             </div>
         </div>
@@ -89,11 +89,11 @@
     <!-- widget recent post -->
     <div class="widget-recent-post">
         <div class="rpost-thumb">
-            <a href="{{ $lang_str }}/docs/theory/assembling_the_order"><img src="/img/docs/assembling_orders_mult.jpg" alt="{{ CustomSiteTranslator::get('Assembling the order', $lang) }}" style="width: 75px;"></a>
+            <a href="{!! $lang_str !!}/docs/theory/assembling_the_order"><img src="/img/docs/assembling_orders_mult.jpg" alt="{!! CustomSiteTranslator::get('Assembling the order', $lang) !!}" style="width: 75px;"></a>
         </div>
         <div class="rpost-content">
             <div class="rpost-title">
-                <h4><a href="{{ $lang_str }}/docs/theory/assembling_the_order">{{ CustomSiteTranslator::get("Assembling the order", $lang) }}</a></h4>
+                <h4><a href="{!! $lang_str !!}/docs/theory/assembling_the_order">{!! CustomSiteTranslator::get("Assembling the order", $lang) !!}</a></h4>
                 <span>02.04.2025</span>
             </div>
         </div>
@@ -101,11 +101,11 @@
     <!-- widget recent post -->
     <div class="widget-recent-post">
         <div class="rpost-thumb">
-            <a href="{{ $lang_str }}/docs/theory/sorting_goods"><img src="/img/docs/sorting_wall.jpg" alt="{{ CustomSiteTranslator::get('Sorting goods', $lang) }}" style="width: 75px;"></a>
+            <a href="{!! $lang_str !!}/docs/theory/sorting_goods"><img src="/img/docs/sorting_wall.jpg" alt="{!! CustomSiteTranslator::get('Sorting goods', $lang) !!}" style="width: 75px;"></a>
         </div>
         <div class="rpost-content">
             <div class="rpost-title">
-                <h4><a href="{{ $lang_str }}/docs/theory/sorting_goods">{{ CustomSiteTranslator::get("Sorting goods", $lang) }}</a></h4>
+                <h4><a href="{!! $lang_str !!}/docs/theory/sorting_goods">{!! CustomSiteTranslator::get("Sorting goods", $lang) !!}</a></h4>
                 <span>02.04.2025</span>
             </div>
         </div>
@@ -113,11 +113,11 @@
     <!-- widget recent post -->
     <div class="widget-recent-post">
         <div class="rpost-thumb">
-            <a href="{{ $lang_str }}/docs/theory/continuity_processes"><img src="/img/docs/continuity_processes_mult.jpg" alt="{{ CustomSiteTranslator::get('Continuity of processes', $lang) }}" style="width: 75px;"></a>
+            <a href="{!! $lang_str !!}/docs/theory/continuity_processes"><img src="/img/docs/continuity_processes_mult.jpg" alt="{!! CustomSiteTranslator::get('Continuity of processes', $lang) !!}" style="width: 75px;"></a>
         </div>
         <div class="rpost-content">
             <div class="rpost-title">
-                <h4><a href="{{ $lang_str }}/docs/theory/continuity_processes">{{ CustomSiteTranslator::get("Continuity of processes", $lang) }}</a></h4>
+                <h4><a href="{!! $lang_str !!}/docs/theory/continuity_processes">{!! CustomSiteTranslator::get("Continuity of processes", $lang) !!}</a></h4>
                 <span>02.04.2025</span>
             </div>
         </div>
